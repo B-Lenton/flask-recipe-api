eb3983cf530b7584cb8c896da6a008dfb5b7697a
Ben Lenton <benlenton@gmail.com>
2022-02-03 19:21:36 +0000

    basic endpoints of API are functioning (tested in postman); database commands and functionality need to be improved.

---
+++ project/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+from database.set_up import *
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/api/recipes', methods=['GET'])
+def api_get_recipes():
+    return jsonify(get_recipes())
+
+@app.route('/api/recipes/<recipe_id>', methods=['GET'])
+def api_get_recipe(recipe_id):
+    return jsonify(get_recipe_by_id(recipe_id))
+
+@app.route('/api/recipes/add',  methods = ['POST'])
+def api_add_recipe():
+    recipe = request.get_json()
+    return jsonify(create_recipe(recipe))
+
+@app.route('/api/recipes/update',  methods = ['PUT'])
+def api_update_recipe():
+    recipe = request.get_json()
+    return jsonify(update_recipe(recipe))
+
+@app.route('/api/recipes/delete/<recipe_id>',  methods = ['DELETE'])
+def api_delete_recipe(recipe_id):
+    recipe = request.get_json()
+    return jsonify(delete_recipe(recipe, recipe_id))
+
+if __name__ == "__main__":
+    #app.debug = True
+    #app.run(debug=True)
+    app.run()
---
+++ project/database/set_up.py
@@ -0,0 +1,287 @@
+import sqlite3
+
+
+def connect_to_db():
+    """
+    Reusable connection to the database.
+    """
+    conn = sqlite3.connect(
+        '/home/ben/Desktop/Just-IT/python/recipe-app/project/database/recipe-app.db'
+    )
+    return conn
+
+def create_users_table():
+    """
+    Create users table.
+    """
+    try:
+        conn = connect_to_db()
+        conn.execute('''
+            CREATE TABLE users (
+                user_id INTEGER PRIMARY KEY NOT NULL,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL,
+                password TEXT NOT NULL
+            );
+        ''')
+        conn.commit()
+        print("Users table created successfully")
+    except:
+        print("Users table creation failed - Maybe table")
+    finally:
+        conn.close()
+
+
+def create_recipes_table():
+    """
+    Create recipes table with a creator FK (= user_id).
+    If a user is deleted, so are their recipes.
+    """
+    try:
+        conn = connect_to_db()
+        conn.execute('''
+            CREATE TABLE recipes (
+                recipe_id INTEGER PRIMARY KEY NOT NULL,
+                recipe_name TEXT NOT NULL,
+                description TEXT NOT NULL,
+                creator INTEGER NOT NULL,
+                CONSTRAINT fk_users
+                    FOREIGN KEY (creator)
+                    REFERENCES users(user_id)
+                    ON DELETE CASCADE
+            );
+        ''')
+        conn.commit()
+        print("Recipes table created successfully")
+    except:
+        print("Recipes table creation failed - Maybe table")
+    finally:
+        conn.close()
+
+
+def create_ingredients_table():
+    """
+    Create ingredients table.
+    """
+    try:
+        conn = connect_to_db()
+        conn.execute('''
+            CREATE TABLE ingredients (
+                ingredient_id INTEGER PRIMARY KEY NOT NULL,
+                ingredient_name TEXT NOT NULL,
+                UNIQUE(ingredient_name)
+            );
+        ''')
+        conn.commit()
+        print("Ingredients table created successfully")
+    except:
+        print("Ingredients table creation failed - Maybe table")
+    finally:
+        conn.close()
+
+
+def create_measurement_units_table():
+    """
+    Create measurement units table.
+    """
+    try:
+        conn = connect_to_db()
+        conn.execute('''
+            CREATE TABLE measurement_units (
+                measurement_id INTEGER PRIMARY KEY NOT NULL,
+                measurement_type TEXT NOT NULL,
+                UNIQUE(measurement_type)
+            );
+        ''')
+        conn.commit()
+        print("Measurement units table created successfully")
+    except:
+        print("Measurement units table creation failed - Maybe table")
+    finally:
+        conn.close()
+
+
+def create_measurement_qty_table():
+    """
+    Create measurement_qty table.
+    """
+    try:
+        conn = connect_to_db()
+        conn.execute('''
+            CREATE TABLE measurement_qty (
+                qty_id INTEGER PRIMARY KEY NOT NULL,
+                qty_amount TEXT NOT NULL,
+                UNIQUE(qty_amount)
+            );
+        ''')
+        conn.commit()
+        print("measurement_qty table created successfully")
+    except:
+        print("measurement_qty table creation failed - Maybe table")
+    finally:
+        conn.close()
+
+
+def create_recipe_ingredients_table():
+    """
+    Create recipe_ingredients table.
+    """
+    try:
+        conn = connect_to_db()
+        conn.execute('''
+            CREATE TABLE recipe_ingredients (
+                recipe_id INTEGER NOT NULL,
+                measurement_id INTEGER NOT NULL,
+                measurement_qty_id INTEGER NOT NULL,
+                ingredient_id INTEGER NOT NULL,
+                FOREIGN KEY(recipe_id)
+                    REFERENCES recipes(recipe_id)
+                    ON DELETE CASCADE,
+                FOREIGN KEY(measurement_id)
+                    REFERENCES measurement_units(measurement_id)
+                    ON DELETE CASCADE,
+                FOREIGN KEY(measurement_qty_id)
+                    REFERENCES measurement_qty(qty_id)
+                    ON DELETE CASCADE,
+                FOREIGN KEY(ingredient_id)
+                    REFERENCES ingredients(ingredient_id)
+                    ON DELETE CASCADE
+            );
+        ''')
+        conn.commit()
+        print("recipe_ingredients table created successfully")
+    except:
+        print("recipe_ingredients table creation failed - Maybe table")
+    finally:
+        conn.close()
+
+
+# create_users_table()
+# create_recipes_table()
+# create_ingredients_table()
+# create_measurement_units_table()
+# create_measurement_qty_table()
+# create_recipe_ingredients_table()
+
+
+def create_recipe(recipe):
+    created_recipe = {}
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        # insert recipe into recipes table
+        cur.execute("INSERT INTO recipes (recipe_name, description, creator) VALUES (?, ?, ?)", (recipe['recipe_name'], recipe['description'], recipe['creator'],))
+
+        # TODO: insert ingredients into ingredients table
+        """
+        # E.G:
+        data = [
+        ('Jane', date(2005, 2, 12)),
+        ('Joe', date(2006, 5, 23)),
+        ('John', date(2010, 10, 3)),
+        ]
+        stmt = "INSERT INTO employees (first_name, hire_date) VALUES (%s, %s)"
+        cursor.executemany(stmt, data)
+        """
+        stmt = "INSERT INTO ..."
+        data = []
+        cur.executemany()
+        conn.commit()
+        created_recipe = get_recipe_by_id(cur.lastrowid)
+    except:
+        conn.rollback()
+
+    finally:
+        conn.close()
+
+    return created_recipe
+
+# works:
+# create_recipe({
+#     "recipe_name": "Vegan Pizza",
+#     "description": "You've never tasted anything like it",
+#     "creator": 1
+# })
+
+
+def get_recipes():
+    recipes = []
+    try:
+        conn = connect_to_db()
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM recipes")
+        rows = cur.fetchall()
+
+        # convert row object to dictionary
+        for i in rows:
+            recipe = {}
+            recipe["recipe_id"] = i["recipe_id"]
+            recipe["recipe_name"] = i["recipe_name"]
+            recipe["description"] = i["description"]
+            recipe["creator"] = i["creator"]
+            recipes.append(recipe)
+    except:
+        recipes = []
+
+    return recipes
+
+
+def get_recipe_by_id(recipe_id):
+    recipe = {}
+    try:
+        conn = connect_to_db()
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM recipes WHERE recipe_id = ?", (recipe_id,))
+        row = cur.fetchone()
+
+        # convert row object to dictionary
+        recipe["recipe_id"] = row["recipe_id"]
+        recipe["recipe_name"] = row["recipe_name"]
+        recipe["description"] = row["description"]
+        recipe["creator"] = row["creator"]
+    except:
+        recipe = {}
+
+    return recipe
+
+
+def update_recipe(recipe):
+    updated_recipe = {}
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute("UPDATE recipes SET recipe_name = ?, description = ? WHERE recipe_id = ? AND creator = ?", (recipe["recipe_name"], recipe["description"], recipe["recipe_id"], recipe["creator"],))
+        conn.commit()
+        #return the recipe
+        updated_recipe = get_recipe_by_id(recipe["recipe_id"])
+    except:
+        conn.rollback()
+        updated_recipe = {}
+    finally:
+        conn.close()
+
+    return updated_recipe
+
+
+def delete_recipe(recipe, recipe_id):
+    message = {}
+    try:
+        conn = connect_to_db()
+        # check that the recipe exists
+        count = conn.execute("SELECT COUNT (*) FROM recipes WHERE recipe_id = ? AND creator = ?", (recipe_id, recipe['creator'],))
+        count = str(list(count)[0])[1:2]
+        if count == "0":
+            message["status"] = "Recipe not found"
+        else:
+            conn.execute("DELETE from recipes WHERE recipe_id = ? AND creator = ?", (recipe_id, recipe['creator'],))
+            conn.commit()
+            message["status"] = "Recipe deleted successfully"
+    except:
+        conn.rollback()
+        message["status"] = "Cannot delete recipe"
+    finally:
+        conn.close()
+
+    return message
