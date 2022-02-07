3f9049f990442372f9a38285f049d77bb2a79407
Ben Lenton <benlenton@gmail.com>
2022-02-07 12:06:15 +0000

    update and delete working; moved onto full text search capabilities. not yet working

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-from database.set_up import *
+from blueprints.blueprint_recipes import *
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -10,7 +11,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 def api_get_recipes():
     return jsonify(get_recipes())
 
-@app.route('/api/recipes/<recipe_id>', methods=['GET'])
+@app.route('/api/recipes/<int:recipe_id>', methods=['GET'])
 def api_get_recipe(recipe_id):
     return jsonify(get_recipe_by_id(recipe_id))
 
@@ -19,16 +20,20 @@ def api_add_recipe():
     recipe = request.get_json()
     return jsonify(create_recipe(recipe))
 
-@app.route('/api/recipes/update',  methods = ['PUT'])
-def api_update_recipe():
+@app.route('/api/recipes/update/<int:recipe_id>',  methods = ['PUT'])
+def api_update_recipe(recipe_id):
     recipe = request.get_json()
-    return jsonify(update_recipe(recipe))
+    return jsonify(update_recipe(recipe, recipe_id))
 
-@app.route('/api/recipes/delete/<recipe_id>',  methods = ['DELETE'])
+@app.route('/api/recipes/delete/<int:recipe_id>',  methods = ['DELETE'])
 def api_delete_recipe(recipe_id):
     recipe = request.get_json()
-    print(recipe)
     return jsonify(delete_recipe(recipe, recipe_id))
+
+@app.route('/api/recipes/search', methods=['POST'])
+def api_search_recipes():
+    query_string = request.query_string.decode() 
+    return jsonify(search_recipes(query_string))
 
 if __name__ == "__main__":
     #app.debug = True
